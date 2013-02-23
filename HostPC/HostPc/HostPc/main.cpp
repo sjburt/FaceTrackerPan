ea5288f473a348f0c8303d8773ef03e215e6e952
Steven Burt <vheissu@gmail.com>
2013-02-22 23:10:42 -0800

    added project

---
+++ HostPC/HostPc/HostPc/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  HostPc
+//
+//  Created by Steven Burt on 2/22/13.
+//  Copyright (c) 2013 UCSC. All rights reserved.
+//
+
+#include <iostream>
+#include <cv.h>
+#include <highgui.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
