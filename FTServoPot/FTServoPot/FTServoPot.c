032c928b1d4ed61b21f3f1200f08c6ed316390ff
steve <vheissu@gmail.com>
2013-02-23 17:01:08 -0800

    added avr project

---
+++ FTServoPot/FTServoPot/FTServoPot.c
@@ -0,0 +1,17 @@
+/*
+ * FTServoPot.c
+ *
+ * Created: 2/23/2013 4:46:11 PM
+ *  Author: Steve
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
